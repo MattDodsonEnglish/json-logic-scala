b46325dc67decf68e1c5066ccbf8072856d19107
Charles-Edouard LADARI <17705729+celadari@users.noreply.github.com>
2021-06-17 01:31:34 +0200

    --wip-- [skip ci]

--- src/main/scala/com/github/celadari/jsonlogicscala/reduce/ReduceLogicConf.scala
+++ src/main/scala/com/github/celadari/jsonlogicscala/reduce/ReduceLogicConf.scala
@@ -1,25 +1,25 @@
 package com.github.celadari.jsonlogicscala.reduce
 
 import scala.collection.mutable
-import com.github.celadari.jsonlogicscala.reduce.defaults.OperatorPlus
+import com.github.celadari.jsonlogicscala.reduce.defaults._
 import com.github.celadari.jsonlogicscala.tree.types.TypeValue
 
 object ReduceLogicConf {
 
   val DEFAULT_OPERATORS_TO_METHODNAME: Map[String, (String, Operator)] = Map(
-    //"<" -> ("$less", OperatorLess),
-    //"<=" -> ("$less$eq", OperatorLessEq),
-    //">" -> ("$greater", OperatorGreater),
-    //">=" -> ("$greater$eq", OperatorGreaterEq),
+    "<" -> ("$less", OperatorLess),
+    "<=" -> ("$less$eq", OperatorLessEq),
+    ">" -> ("$greater", OperatorGreater),
+    ">=" -> ("$greater$eq", OperatorGreaterEq),
     //"<<" -> ("$less$less", OperatorLessLess),
     //">>" -> ("$greater$greater", OperatorGreaterGreater),
     //">>>" -> ("$greater$greater$greater", OperatorGreaterGreaterGreater),
-    "+" -> ("$plus", OperatorPlus)
-    //"-" -> ("$minus", OperatorMinus),
-    //"*" -> ("$times", OperatorTimes),
-    //"/" -> ("$div", OperatorDiv),
-    //"%" -> ("$percent", OperatorModulo),
-    //"^" -> ("$up", OperatorXor),
+    "+" -> ("$plus", OperatorPlus),
+    "-" -> ("$minus", OperatorMinus),
+    "*" -> ("$times", OperatorTimes),
+    "/" -> ("$div", OperatorDiv),
+    "%" -> ("$percent", OperatorModulo),
+    "^" -> ("$up", OperatorXor)
     //"|" -> ("$bar", OperatorBar),
     //"or" -> ("$bar$bar", OperatorOr),
     //"&" -> ("$amp", OperatorAmp),
