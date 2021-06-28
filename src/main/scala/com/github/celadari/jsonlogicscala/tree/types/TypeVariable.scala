a879c0abda1f369bdda85fe4be001b084db7f87e
Charles-Edouard LADARI <17705729+celadari@users.noreply.github.com>
2021-06-29 00:44:16 +0200

    --wip-- [skip ci]

--- src/main/scala/com/github/celadari/jsonlogicscala/tree/types/TypeVariable.scala
+++
@@ -1,8 +0,0 @@
-package com.github.celadari.jsonlogicscala.tree.types
-
-object TypeVariable {
-  val CODENAME_TYPE: String = "composeVariable"
-
-}
-
-case class TypeVariable(variableName: String) extends TypeValue(TypeVariable.CODENAME_TYPE)
