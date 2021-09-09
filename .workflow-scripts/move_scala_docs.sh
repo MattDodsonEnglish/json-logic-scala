41ed3420f2efd7420eaacd98589544d8a7de03cb
Charles-Edouard LADARI <17705729+celadari@users.noreply.github.com>
2021-09-09 22:54:06 +0200

    Check python scripts 33

--- .workflow-scripts/move_scala_docs.sh
+++ .workflow-scripts/move_scala_docs.sh
@@ -1,12 +1,9 @@
 
-echo "$scala_versions"
-echo "$api_version"
 
 # shellcheck disable=SC2154
 for scala_version in $scala_versions
 do
-#  mkdir -p "$2/scala-$scala_version"
-#  mv "$1/scala-$scala_version/$api_version" "$2/scala-$scala_version/"
-  python "update_json_api_versions $3 $api_version --scala-versions $scala_versions"
+  mkdir -p "$2/scala-$scala_version"
+  mv "$1/scala-$scala_version/$api_version" "$2/scala-$scala_version/"
+  python "$3/update_json_api_versions $4 $api_version --scala-versions $scala_versions"
 done
-
--- .workflow-scripts/put_scala_docs_aside.sh
+++ .workflow-scripts/put_scala_docs_aside.sh
@@ -4,15 +4,11 @@ scala_versions=$(ls "$1" | grep -Eo "[0-9]\.[0-9][0-9]")
 api_version=$(sbt version | sed -r 's/[^ \t\r\n\v\f0-9\.i?n?f?o?]+ (.+)/APP_VERSION\1/' | sed -n '6p' | grep -Po '(?<=PP_VERSION)(.+)')
 
 
-
-echo "$scala_versions"
-echo "$api_version"
-
 export scala_versions
 export api_version
 
-#for scala_version in $scala_versions
-#do
-#  mkdir -p "$2/scala-$scala_version/$api_version"
-#  mv "$1/scala-$scala_version/api/*" "$2/scala-$scala_version/$api_version/"
-#done
+for scala_version in $scala_versions
+do
+  mkdir -p "$2/scala-$scala_version/$api_version"
+  mv "$1/scala-$scala_version/api/*" "$2/scala-$scala_version/$api_version/"
+done
