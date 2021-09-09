94ce7eba6736ba31f10692721a869f55f7a55a4c
Charles-Edouard LADARI <17705729+celadari@users.noreply.github.com>
2021-09-09 23:47:20 +0200

    Check python scripts 43

--- .workflow-scripts/move_scala_docs.sh
+++ .workflow-scripts/move_scala_docs.sh
@@ -8,5 +8,5 @@ for scala_version in $scala_versions
 do
   mkdir -p "$2/scala-$scala_version"
   mv "$1/scala-$scala_version/$api_version" "$2/scala-$scala_version/"
-  python "$3/update_json_api_versions.py $4 $api_version --scala-versions $scala_versions"
+  python "$3/update_json_api_versions.py" "$4" "$api_version" --scala-versions "$scala_versions"
 done
