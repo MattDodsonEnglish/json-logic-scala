594118a98d80e7aa1a2fa0d116f3705e9b7fecd3
Charles-Edouard LADARI <17705729+celadari@users.noreply.github.com>
2021-09-09 16:37:46 +0200

    Check python scripts 21

--- .workflow-scripts/put_scala_docs_aside.sh
+++ .workflow-scripts/put_scala_docs_aside.sh
@@ -5,7 +5,8 @@ scala_versions=$(ls "$1" | grep -Eo "[0-9]\.[0-9][0-9]")
 api_version=$(sbt version | tail -n1 | sed -r 's/[^ \t\r\n\v\f0-9\.]+ (.+)/\1/')
 
 (sbt version | tail -n1)
-(sbt version | sed -r 's/[^ \t\r\n\v\f0-9\.]+ (.+)/APP_VERSION\1/')
+(sbt version | sed -r 's/[^ \t\r\n\v\f0-9\.i?n?f?o?]+ (.+)/APP_VERSION\1/')
+(sbt version | sed -r 's/[^ \t\r\n\v\f0-9\.i?n?f?o?]+ (.+)/\1/')
 (sbt version | sed -r 's/[^ \t\r\n\v\f0-9\.]+ (.+)/APP_VERSION\1/' | sed -r 's/^A?P?P?_?V?E?R?S?I?O?N?info\] (.+)/\1/' | grep -nE '*')
 
 echo "$scala_versions"
