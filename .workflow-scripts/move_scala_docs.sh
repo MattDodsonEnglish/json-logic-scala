25973a309e4ebfd90d0dfb8e5b22a5b34fbe4122
Charles-Edouard LADARI <17705729+celadari@users.noreply.github.com>
2021-09-10 18:49:16 +0200

    Check python scripts 100

--- .workflow-scripts/move_scala_docs.sh
+++ .workflow-scripts/move_scala_docs.sh
@@ -7,6 +7,6 @@ do
   rm -rf "$2/scala-$scala_version/latest"
   cp -r "$1/scala-$scala_version/$api_version" "$2/scala-$scala_version/latest"
   mv "$1/scala-$scala_version/$api_version" "$2/scala-$scala_version/"
-  api_version_unicode=$(echo "$api_version" | uni2ascii -a U -q)
+  api_version_unicode=$(echo "$api_version" | ansi2txt)
   python "$3/update_json_api_versions.py" "$4" "$api_version_unicode" --scala-versions "$scala_versions"
 done
