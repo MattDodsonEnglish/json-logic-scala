b788e1d0a10485c7451eeb628a83469b054e5906
Charles-Edouard LADARI <17705729+celadari@users.noreply.github.com>
2021-09-10 18:01:12 +0200

    Check python scripts 82

--- .workflow-scripts/put_scala_docs_aside.sh
+++ .workflow-scripts/put_scala_docs_aside.sh
@@ -1,7 +1,7 @@
 
 
 # shellcheck disable=SC2012
-scala_versions=$(ls "$1" | ascii2uni -a U -q | grep -Eo "[0-9]\.[0-9][0-9]")
+scala_versions=$(ls "$1" | ascii2uni | grep -Eo "[0-9]\.[0-9][0-9]")
 api_version=$(sbt version | ascii2uni -a U -q | tail -n2 | head -n1 | sed 's/[info] //')
 
 export scala_versions
